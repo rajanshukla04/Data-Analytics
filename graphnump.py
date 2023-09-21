c8559b2a8a391f8e44328caec8e42f62ff139458
Rajan Shukla <rajan93992@gmail.com>
2023-09-21 10:48:53 +0530

    graph

---
+++ graphnump.py
@@ -0,0 +1,20 @@
+import  numpy as np
+import matplotlib.pyplot as plt
+# x=np.arange(0,3*np.pi,0.1)
+#
+#   # y=np.sin(x)  # this is sine graph
+# y=np.cos(x)    # cos  is graph
+# plt.plot(x,y)
+#
+# plt.show()
+#
+
+
+#
+
+ar=np.array([1,2,3])
+print(np.exp(ar))
+
+print(np.log(ar))
+print(np.log10(ar))
+
